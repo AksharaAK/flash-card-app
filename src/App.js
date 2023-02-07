d273085355f1312eb2c4a7c133f69e6bfa4a4e80
AksharaAK <akshann1606@gmail.com>
2023-02-07 12:30:43 +0000

    feat: create new timer context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./Components/Header";
 import { ContinentPicker } from "./Components/ContinentPicker";
 import Quiz from "./Components/Quiz";
+import SettingsContext from "./context/SettingsContext";
 
 export default function App() {
   const [continent, setContinent] = useState("america");
@@ -9,14 +10,27 @@ export default function App() {
     setContinent(e.target.value);
   };
 
+  const [workMins, setWorkMins] = useState(25);
+  const [breakMins, setBreakMins] = useState(5);
+
   return (
     <div class={`bg-cover bg-center my-bg-hero`}>
       <Header />
-      <ContinentPicker
-        continent={continent}
-        continentChangeHandler={continentChangeHandler}
-      />
-      <Quiz continent={continent} />
+      <SettingsContext.Provider
+        value={{
+          workMins,
+          breakMins,
+          setWorkMins,
+          setBreakMins,
+        }}
+      >
+        <ContinentPicker
+          continent={continent}
+          continentChangeHandler={continentChangeHandler}
+        />
+
+        <Quiz continent={continent} />
+      </SettingsContext.Provider>
     </div>
   );
 }
